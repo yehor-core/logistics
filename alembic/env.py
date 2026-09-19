81e712368b3c3726bc1c386b0c9325d20d0b61ef
hannahaw <mshedris@gmail.com>
2026-09-19 16:32:23 +0300

    fix: resolve pre-commit linting issues in alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -6,14 +6,13 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+from src.config import settings
+from src.db.base import Base
+from src.db.models import Post, Source  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
-from src.config import settings
-from src.db.base import Base
-from src.db.models import Post, Source  # noqa: F401
 
 config.set_main_option("sqlalchemy.url", settings.database_url.get_secret_value())
 target_metadata = Base.metadata
